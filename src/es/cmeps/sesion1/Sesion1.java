9006a7eea7d0d71901cbd39ef3d9d04a345a9b75
jccastrocc <jccastroc.develop@gmail.com>
2024-09-29 13:18:46 +0200

    Commit paso 3

--- src/es/cmeps/sesion1/Sesion1.java
+++ src/es/cmeps/sesion1/Sesion1.java
@@ -5,6 +5,7 @@ public class Sesion1 {
 	public static void main(String[] args) {
 		System.out.println("Sesion 1, primer commit");
 		System.out.println("Sesion 1, paso 2");
+		System.out.println("Sesion 1, paso 3");
 
 	}
 
