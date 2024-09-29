12d78a213a2c215f790f28dd0ae86b49038f4a70
jccastrocc <jccastroc.develop@gmail.com>
2024-09-29 13:25:20 +0200

    Commit paso 6

--- src/es/cmeps/sesion1/Sesion1.java
+++ src/es/cmeps/sesion1/Sesion1.java
@@ -6,6 +6,7 @@ public class Sesion1 {
 		System.out.println("Sesion 1, primer commit");
 		System.out.println("Sesion 1, paso 2");
 		System.out.println("Sesion 1, paso 3");
+		System.out.println("Sesion 1, paso 6");// ya tenemos mergeados el paso 2 y 3 en master
 
 	}
 
